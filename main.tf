47beb7669beb67ec03e9e47760954ac89f1a53bd
Princy Kshirsagar <112242191+princyi@users.noreply.github.com>
2024-10-12 12:02:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "s3:GetObject"
+        Effect = "Allow"
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+        Principal = "*"
+      },
+    ]
+  })
+}
